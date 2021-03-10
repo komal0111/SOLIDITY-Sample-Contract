dde522a9212ed0a5d7e2ab4b8ece6b18a988b3f3
komal0111 <77234031+komal0111@users.noreply.github.com>
2021-03-10 13:27:24 +0530

    Update Timestamp.sol
    SPDX(Software Package Data Exchange ) -GPL3.0 means :GNU General Public License v3.0 or later

--- Timestamp.sol
+++ Timestamp.sol
@@ -1,3 +1,6 @@
+/**SPDX(Software Package Data Exchange ) -GPL3.0 means :GNU General Public License v3.0 or later
+SPDX is used in free and open or collaborative software, data, hardware, or documentation**/
+
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.7.0;
 contract Timestamp

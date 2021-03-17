d6da2219bdf9938c662a8878280c27ec3af1784f
komal0111 <77234031+komal0111@users.noreply.github.com>
2021-03-17 16:27:20 +0530

    Update getBalance.sol

--- getBalance.sol
+++ getBalance.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.1;
+
 contract MyGetContract
 
 {
@@ -7,18 +8,22 @@ contract MyGetContract
     address owner;
     
     constructor () public
-    {
+     {
 	// it store owner address
         owner=msg.sender;
-    }
+     }
 
+
+     /* if You call this function then it execute this code hiddenly
+    address(this).balance=address(this).balance+msg.value */
+    
     function receiveDepositAmount()payable public
-    {
-        //if You call this function then it execute this code hiddenly
-        //address(this).balance=address(this).balance+msg.value
+     {
+        
     }
+    
     function getBalance() public view returns (uint)
-   	 {
-    return address(this).balance;
-	}
+    {
+    	return address(this).balance;
+   }
 }
